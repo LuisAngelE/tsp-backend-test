54c4e1a85083f5a5437cbc208e0d4fe8e61d3a6d
LuisAngelE <luisangelem.dp@gmail.com>
2025-05-22 23:32:46 -0600

    Validation

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
     const port = process.env.PORT ?? 3000;
     const prefix = 'api';
+
     const app = await NestFactory.create(TspModule);
-    await app.listen(port);
+
+    // Establecer prefijo global
     app.setGlobalPrefix(prefix);
+
+    // Aplicar validación global con class-validator
     app.useGlobalPipes(new ValidationPipe());
+
+    await app.listen(port);
     Logger.log(`Application is running on: http://localhost:${port}/${prefix}`);
 }
 
